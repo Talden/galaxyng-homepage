a9528ed626400c08fff4819ad1ece2b72dbe8274
Ken Weinert <madcoder42@users.sourceforge.net>
2005-01-23 00:30:35 +0000

    minor correction, a comment in the head file, some variable name clarification

--- head.php
+++ head.php
@@ -1,9 +1,17 @@
 <?php
 	// change this to your GalaxyNG server directory
+	// note that the name of the executable *is not* in the path
+	// All we need is the path to it, the script handles the rest
+
 	$NGpath="/home/gng/Games";
-   putenv("GALAXYNGHOME=$NGpath");
+	putenv("GALAXYNGHOME=$NGpath");
 ?>
 
 
-<!-- you can customize this, and it will not be overwritten when you upgrade. -->
+<!-- you can customize this, and it will not be overwritten when you upgrade.
+     what normally goes in here is the contents of the <head></head> portion
+     of the HTML file. So you can, for example, put in CSS links and such, a
+     title for your page, etc.  Just prior to this you have the <html> tag
+     and following this will be either the <form> tag or a <p> tag with the
+     contents of the report or the forecast. -->
 
