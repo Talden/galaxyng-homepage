11ec2ae1df55123859a04133416a0813cec0543b
Ken Weinert <madcoder42@users.sourceforge.net>
2005-01-23 01:42:45 +0000

    changes to download the resulting text file from either the report or forecast

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US">
-
 <?php
 
-include "head.php";
+include "env.php";
 
 if ($type and $orders) {  
+
+	header('Content-type: text/plain');
 
 	$tmpfname = tempnam("/tmp", "galaxyng");
 	
@@ -15,6 +12,8 @@ if ($type and $orders) {
 	
 	$orders = explode("\n", $_POST["orders"]);
 	
+	$line1 = explode(" ", $orders[0]);
+
 	for ($i = 0; $i < count($orders); $i++) {
 	  $line = rtrim($orders[$i]);
 	  fwrite($fp, $line); 
@@ -23,24 +22,30 @@ if ($type and $orders) {
 	
 	fclose($fp);
 	
-	echo "<p><pre>\n";
-	
 	switch ($type) {
 		case "cmd_orders":
+			$filename=$line1[2] . "_forecast_" . $line1[4] . ".txt";
+			header("Content-disposition: attachment; filename=\"$filename\"");
 			passthru("$NGpath/galaxyng -webcheck < $tmpfname");
 		break;
 		case "cmd_report":
+			$filename=$line1[2] . "_report_" . $line1[4] . ".txt";
+			header("Content-disposition: attachment; filename=\"$filename\"");
 			passthru("$NGpath/galaxyng -webreport < $tmpfname");
 		break;
 	}
 	
 	$command = "rm -f " . $tmpfname;
 	system($command);
+	exit;
 	
-	echo "\n</pre></p>";
-
 } else { ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
+
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US">
+	<?php include "head.php"; ?>
 	<form action="<?php echo $PHP_SELF; ?>" method="post">
 	<p><input type="radio" name="type" value="cmd_orders" checked>Orders <input type="radio" name="type" value="cmd_report"> Report
 	<br/><textarea name="orders" rows="10" cols="80"></textarea>
