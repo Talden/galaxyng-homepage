11799b0ffcc461cfbe2a397b20272e309d6bb3f1
Chris Shaffer <christhecat@users.sourceforge.net>
2006-05-06 21:36:49 +0000

    Updated to account for register_globals being off by default.  Also changed confusing double use of variable name.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include "env.php";
 
-if ($type and $orders) {  
+if ($_POST["type"] and $_POST["orders"]) {
 
 	header('Content-type: text/plain');
 
@@ -10,35 +10,34 @@ if ($type and $orders) {
 	
 	$fp = fopen($tmpfname, "w"); 
 	
-	$orders = explode("\n", $_POST["orders"]);
+	$lines = explode("\n", $_POST["orders"]);
 	
-	$line1 = preg_split("[\s]", $orders[0], -1, PREG_SPLIT_NO_EMPTY);
-
-	for ($i = 0; $i < count($orders); $i++) {
-	  $line = rtrim(str_replace("\\", "", $orders[$i]));
+	$line1 = preg_split("[\s]", $lines[0], -1, PREG_SPLIT_NO_EMPTY);
+	
+	for ($i = 0; $i < count($lines); $i++) {
+	  $line = rtrim(str_replace("\\", "", "$lines[$i]"));
 	  fwrite($fp, $line); 
 	  fwrite($fp, "\n");
 	}
 	
 	fclose($fp);
 	
-	switch ($type) {
-		case "cmd_orders":
-			$filename=$line[1] . "_" . $line1[2] . "_forecast_" . $line1[4] . ".txt";
+	switch ($_POST["type"]) {
+		case "order":
+			$filename=$line1[1] . "_" . $line1[2] . "_forecast_" . $line1[4] . ".txt";
 			header("Content-disposition: attachment; filename=\"$filename\"");
-			passthru("$NGpath/galaxyng -webcheck < $tmpfname | awk 'sub(\"$\", \"\\r\")'");
+			passthru("$NGpath/galaxyng -webcheck < $tmpfname  | awk 'sub(\"$\", \"\\r\")'");
 		break;
-		case "cmd_report":
-			$filename=$line[1] . "_" . $line1[2] . "_report_" . $line1[4] . ".txt";
+		case "report":
+			$filename=$line1[1] . "_" . $line1[2] . "_report_" . $line1[4] . ".txt";
 			header("Content-disposition: attachment; filename=\"$filename\"");
 			passthru("$NGpath/galaxyng -webreport < $tmpfname | awk 'sub(\"$\", \"\\r\")'");
 		break;
 	}
 	
-	$command = "rm -f " . $tmpfname;
+	$command = "rm -f" . $tmpfname;
 	system($command);
-	exit;
-	
+
 } else { ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
